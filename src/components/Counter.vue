dbcc65b66f69f45842f018bf566fdfc653637350
Marina <marinaandjelkovic456@gmail.com>
2019-03-27 19:49:40 +0100

    spreadovana mutacija i pozvana

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,11 +3,12 @@
         <h1>Counter Component</h1>
         <div>from the component directly: {{ count }}</div>
         <div>from mapGetters: {{ safelyStoredNumber }}</div>
+        <div><button @click.prevent="incrementStoredNumber">Click me</button></div>
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations } from 'vuex'
 
 export default {
     data() {
@@ -18,9 +19,17 @@ export default {
     methods: {
         increment() {
             this.count++
-        }
+        }, 
+
+        //spread operator!!!
+        ...mapMutations([
+            //ime mutacije
+            'incrementStoredNumber',
+            'setStoredNumber'
+        ])
     }, 
     computed: {
+        //spread operator!!!
         ...mapGetters([
             // ime gettera 
             'safelyStoredNumber'
